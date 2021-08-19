b2872483bfbed9d4d867f8c30f95e8fbd1b7116d
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-08-20 00:00:41 +0100

    first commit

---
+++ src/main/java/com/geekykel/ethereum/EthereumBlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.geekykel.ethereum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumBlockchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EthereumBlockchainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geekykel/ethereum/EthereumBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekykel.ethereum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthereumBlockchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
